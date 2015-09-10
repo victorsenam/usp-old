592e62602c824804278255fc54e3ec68f8b9ed3e
Victor Sena Molero <victorsenam@gmail.com>
2015-09-10 17:24:39 -0300

    Mudando codigo de maquina

--- mac300/ep1/main.cpp
+++ mac300/ep1/main.cpp
@@ -2,6 +2,8 @@
 
 const int nmax = 700;
 
+void printmat (int n, double A[][nmax], bool (*shouldprint)(int, int));
+
 int chocol (int n, double A[][nmax]);
 int forwcol (int n, double A[][nmax], double b[]);
 int backcol (int n, double A[][nmax], double b[], int trans);
@@ -16,11 +18,19 @@ int sscol (int n, double A[][nmax], int p[], double b[]);
 int lurow (int n, double A[][nmax], int p[]);
 int ssrow (int n, double A[][nmax], int p[], double b[]);
 
+void printmat (int n, double A[][nmax], bool (*shouldprint)(int, int));
+
 int main () {
     int n;
+    int x, y;
     double A[nmax][nmax];
     double b[nmax];
 
     scanf("%d", &n);
-    for 
+    for (int i = 0; i < n*n; i++)
+        scanf("%d %d %lf", &x, &y, &A[x-1][y-1]);
+    for (int i = 0; i < n; i++)
+        scanf("%lf", b+i);
+
+    printmat (n, A, ([](int i, int j) -> bool { return i < j; }));
 }
