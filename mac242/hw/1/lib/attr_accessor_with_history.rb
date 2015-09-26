992f8b4bb07b2ee506eae7b4051c5de4a21691a3
Victor Sena Molero <victorsenam@gmail.com>
2015-09-26 11:36:18 -0300

    attr_accessor

--- mac242/hw/1/lib/attr_accessor_with_history.rb
+++ mac242/hw/1/lib/attr_accessor_with_history.rb
@@ -1,10 +1,18 @@
+require "pp"
+
 class Class
   def attr_accessor_with_history(attr_name)
     attr_name = attr_name.to_s # make sure it's a string
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
-      # YOUR CODE HERE
+      def #{attr_name}=(val)
+        if !defined? @#{attr_name}_history
+          @#{attr_name}_history = []
+        end
+        @#{attr_name}_history.push(@#{attr_name})
+        @#{attr_name} = val
+      end
     }
   end
 end
