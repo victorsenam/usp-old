3879599286e45a13e76f2ced10a901f55d95ad4b
Victor Sena Molero <victorsenam@gmail.com>
2015-09-26 11:03:33 -0300

    Rock, Paper, Scissors

--- mac242/hw/1/lib/rock_paper_scissors.rb
+++ mac242/hw/1/lib/rock_paper_scissors.rb
@@ -1,14 +1,31 @@
+require "pp"
+
 class RockPaperScissors
+  @@plays = ["R","P","S"]
 
   # Exceptions this class can raise:
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    raise NoSuchStrategyError, "Strategy must be one of R,P,S" unless (player1.length > 1 && @@plays.include?(player1[1]) == true)
+    raise NoSuchStrategyError, "Strategy must be one of R,P,S" unless (player2.length > 1 && @@plays.include?(player2[1]) == true)
+
+    hk1 = @@plays.index(player1[1])
+    hk2 = @@plays.index(player2[1])
+
+    if (hk2 == hk1+1 || hk2+2 == hk1)
+      return player2
+    else
+      return player1
+    end
   end
 
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
+    if (@@plays.include?tournament[1])
+      return tournament
+    else
+      return winner(tournament_winner(tournament[0]), tournament_winner(tournament[1]))
+    end
   end
 
 end
