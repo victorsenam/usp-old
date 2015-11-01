d6ec6c9520080a7efa705e5943455b08f78414fb
Victor Sena Molero <victorsenam@gmail.com>
2015-11-01 16:42:01 -0200

    Nem testei, mas ta completo teoricamente

--- mac300/ep2/codigo/main.cpp
+++ mac300/ep2/codigo/main.cpp
@@ -1,94 +1,186 @@
+// TODO: Tirar funções de print
+
+
+
+// Victor Sena Molero - 8941317
+
 #include <cstdio>
 #include <cstdlib>
 #include <algorithm>
+#include <cassert>
 
+// ==== CLASSES ======
+/* Linked List for Sparse Matrix Representation */
 class linked_list {
 public:
+    double value;
+    int position;
     linked_list * next;
-    double val;
-    int col;
 
-    linked_list() {
+    linked_list(int pos, double x) {
+        value = x;
+        position = pos;
         next = NULL;
-    };
+    }
+
+    ~linked_list() {
+        if (next != NULL)
+            delete next;
+    }
+    void print () {
+        linked_list * el = this;
+
+        while (el != NULL) {
+            printf("(%d, %lf) ", el->position, el->value);
+            el = el->next;
+        }
+        printf("\n");
+    }
 };
 
-class input_els {
+/* Vector Representation */
+class vector {
 public:
-    int lin, col;
-    double val;
+    int size;
+    double * vals;
 
-    bool operator < (const input_els & other) const {
-        if (lin != other.lin)
-            return lin < other.lin;
-        if (col != other.col)
-            return col > other.col;
-        return val < other.val;
+    vector(int n) {
+        size = n;
+        vals = (double *) malloc(size*sizeof(double));
+    }
+
+    ~vector() {
+        free(vals);
+    }
+
+    double & operator [] (int i) {
+        return vals[i];
+    }
+
+    void print() {
+        for (int i = 0; i < size; i++)
+            printf("%f ", vals[i]);
+        printf("\n");
+    }
+
+    double operator * (const vector & other) const {
+        double res = 0.0;
+        for (int i = 0; i < size; i++)
+            res += vals[i] * other[i];
     }
 };
 
-void matrix_vector_product (const linked_list ** & A, const double * & b, double * & res, int n) {
-    for (int i = 0; i < n; i++) {
-        res[i] = 0.0;
+/* Sparse Matrix Representation */
+class matrix {
+public:
+    int dimension;
+    linked_list ** mat;
 
-        linked_list * el = A[i];
-        while (el != NULL)
-            res[i] += (el->val)*b[el->col];
+    matrix(int n) {
+        dimension = n;
+        mat = (linked_list **) malloc(dimension*(sizeof(linked_list *)));
+        for (int i = 0; i < dimension; i++)
+            mat[i] = (linked_list *) NULL;
     }
-}
+    
+    ~matrix() {
+        for (int i = 0; i < dimension; i++)
+            if (mat[i] != NULL)
+                delete mat[i];
+        free(mat);
+    }
 
-int get_input (linked_list ** & mat, int * & b) {
-    int n, m;             // dimensões da matriz
-    input_els * input;    // elementos da entrada
+    linked_list * operator [] (int i) {
+        return mat[i];
+    }
+
+    void insertItem (int line, int column, double value) {
+        linked_list * el = new linked_list(column, value);   
+        el->next = mat[line];
+        mat[line] = el;
+    }
+    
+    void print () {
+        for (int i = 0; i < dimension; i++) {
+            if (mat[i] == NULL)
+                printf("line %d nil\n", i);
+            else
+                mat[i]->print();
+        }
+    }
+
+    void product (vector & r, vector & b) {
+        assert(b.size == dimension);
+        assert(r.size == dimension);
+        for (int i = 0; i < dimension; i++) {
+            r[i] = 0;
+            linked_list * el = mat[i];
+            while (el != NULL) {
+                r[i] += el->value*(b[el->position]);
+                el = el->next;
+            }
+        }
+    }
+};
+
+// ==== FUNCTIONS ====
+void getInput(matrix * & A, vector * & b) {
+    int n, m;
     scanf("%d %d", &n, &m);
 
-    mat = (linked_list **) malloc(n*sizeof(linked_list *));
-    vet = (int *) malloc(n*sizeof(int));
-    input = (input_els *) malloc(m*sizeof(input_els));
-
-    for (int i = 0; i < n; i++)
-        mat[i] = (linked_list *) NULL;
-
-    for (int i = 0; i < m; i++)
-        scanf("%d %d %lf", &input[i].lin, &input[i].col, &input[i].val);
-
-    std::sort(input, input+m);
+    A = new matrix(n);
+    b = new vector(n);
 
     for (int i = 0; i < m; i++) {
-        linked_list * aux = (linked_list *) malloc(sizeof(linked_list));
-        aux->col = input[i].col;
-        aux->val = input[i].val;
-        aux->next = mat[input[i].lin];
-        mat[input[i].lin] = aux;
+        int lin, col;
+        double val;
+        scanf("%d %d %lf", &lin, &col, &val);
+        
+        A->insertItem(lin, col, val);
     }
 
-    for (int i = 0; i < n; i++)
-        scanf("lf", b+i);
-
-    free(input);
-    return n;
+    for (int i = 0; i < n; i++) {
+        double aux;
+        scanf("%lf", b->vals+i);
+    }
 }
 
-void conjugate_gradients (const linked_list ** & A, double * & r, const int n) {
-    double * x = (double *) malloc(n*sizeof(double));
-    double * d = (double *) malloc(n*sizeof(double));
+// ==== MAIN =========
+void conjugateGradients(matrix & A, vector & r) {
+    vector x(r->size);
+    vector d(r->size);
+    vector g(r->size);
+    double lastNorm = (r*r);
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < r->size; i++) {
         x[i] = 0;
         d[i] = r[i];
     }
 
-    
+    for (int k = 0; k < r->size; k++) {
+        A->product(g, d);
+        double alpha = lastNorm/(d*g);
+        for (int i = 0; i < r->size; i++) {
+            x[i] += alpha*d[i];
+            r[i] -= alpha*g[i];
+        }
+        double beta = lastNorm;
+        lastNorm = r*r;
+        beta /= lastNorm;
+        for (int i = 0; i < r->size; i++)
+            d[i] = r[i] + beta*d[i];
+    }
 }
 
 int main () {
-    int n;                   // n
-    linked_list ** A;        // matriz de entrada A
-    double * b;              // vetor de entrada b
+    matrix * A;
+    vector * b;
 
-    // recebe a entrada
-    n = get_input(A, b);
+    getInput(A, b);
+    conjugateGradients(*A, *b);
 
-    // calcula x em b
-    
+    b->print();
+
+    delete b;
+    delete A;
 }
