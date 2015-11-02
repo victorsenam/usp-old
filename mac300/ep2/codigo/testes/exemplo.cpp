f14f7fbd3376035d045d6f08a662c534105cf616
Victor Sena Molero <victorsenam@gmail.com>
2015-11-02 08:27:25 -0200

    Readicionando Testes

---
+++ mac300/ep2/codigo/testes/exemplo.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double eps = 1e-9;
+
+int main () {
+    srand(time(NULL)); rand(); rand();
+    int n;
+    double tau;
+    scanf("%d %lf", &n, &tau);
+
+    for (int i = 0; i < n; i++)
+        printf("%d %d %.7f\n", i, i, 1.0);
+    
+    int cnt = 0;
+    for (int i = 1; i < n; i++) {
+        for (int j = i+1; j < n; j++) {
+            double g = rand(); // D-O-double G!
+            g -= .5*RAND_MAX;
+            g /= RAND_MAX;
+            if (g > tau || g < -tau)
+                continue;
+            printf("%d %d %.7f\n", i, j, g);
+            cnt++;
+        }
+    }
+    printf("%d\n", cnt);
+}
