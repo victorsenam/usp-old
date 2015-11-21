03116776eb97f217213179768cdfbbe225d808a6
Victor Sena Molero <victorsenam@gmail.com>
2015-11-20 22:01:16 -0200

    hummm sono...

--- mac300/ep3/codigo/main.cpp
+++ mac300/ep3/codigo/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
-#include <stdio.h>
+
+const double eps = 1e-9;
 
 void recebe_entrada (int * n, int * m, double ** A, double * b) {
     int i;              // iterador
@@ -23,59 +24,75 @@ void recebe_entrada (int * n, int * m, double ** A, double * b) {
         scanf("%lf", b+i);
 }
 
-void aloca_auxiliares (int m, int * p, double * norma, double * escala, double * gamma) {
+void aloca_auxiliares (int m, int * p, double * norma, double * gamma) {
     p = (int *) malloc(sizeof(int *)*(*m));   
     norma = (double *) malloc(sizeof(double *)*(*m));   
-    escala = (double *) malloc(sizeof(double *)*(*m));   
     gamma = (double *) malloc(sizeof(double *)*(*m));   
 }
 
-void pre_processa (int n, int m, double ** A, int * p, double * norma, double * escala) {
+double pre_processa (int n, int m, double ** A, int * p, double * norma) {
     // inicializando
     for (int i = 0; i < m; i++)
         p[i] = i;
     for (int i = 0; i < m; i++)
-        norma[i] = escala[i] = 0;
+        norma[i] = 0;
+    double escala = 0;
 
-    // calculando norma maxima de cada coluna
-    for (int i = 0; i < n; i++) {
+    // calculando o módulo máximo
+    for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
-            escala[j] = std::max(escala[j], std::abs(A[i][j]));
-    }
+            escala = std::max(escala, std::abs(A[i][j]));
 
     // re-escalando as colunas enquanto calcula norma dois ao quadrado
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            A[i][j] /= escala[j];
+            A[i][j] /= escala;
             norma[j] += A[i][j]*A[i][j];
         }
     }
 }
 
-int decompoe_qr (int n, int m, double ** A, int * p, double * norma, double * escala, double * gamma) {
-    int posto = 0;
+int decompoe_qr (int n, int m, double ** A, int * p, double * norma, double * gamma) {
+    double aux = (double *) malloc(sizeof(double)*n);
 
     for (int k = 0; k < m; k++) {
         // buscando a coluna de maior norma
         int maxi = k;
         for (int i = k; i < m; i++)
-            if (std::abs(norma[p[i]]) > std::abs(norma[p[maxi]])
+            if (norma[p[i]] > norma[p[maxi]])
                 maxi = i;
 
-        swap(p[k], p[maxi]);
+        if (norma[maxi] < eps)
+            return k;
         
-        if (A[k][p[k]] > 0)
-            A[k][p[k]] += norma[p[k]];
-        else
-            A[k][p[k]] -= norma[p[k]];
-        gamma[p[k]] = A[k][p[k]]*A[k][p[k]];
+        for (int i = 0; i < n; i++)
+            swap(A[i][k], A[i][maxi]);
+        swap(norma[k], norma[maxi]);
+        p[k] = maxi;
+        
+        double tau = norma[k];
+        if (A[k][k] < 0)
+            tau = -tau;
 
-        // colocando u na coluna k
+        gamma[k] = (A[k][k] + tau)/tau;
+        for (int i = k; i < n; i++)
+            A[i][k] /= A[k][k]+tau;
+
+        for (int i = k+1; i < m; i++)
+            gamma[i] = 0;
+
         for (int i = k+1; i < n; i++)
-            A[i][p[k]] /= A[k][p[k]];
+            for (int j = k+1; j < m; j++)
+                gamma[j] += aux[i]*gamma[k]*A[i][j];
 
-        
+        for (int i = k+1; i < n; i++)
+            for (int j = k+1; j < m; j++)
+                A[i][j] -= A[i][k]*gamma[j];
+
+        A[k][k] = -tau;
     }
+
+    return m;
 }
 
 int main () {
@@ -84,12 +101,14 @@ int main () {
     double * b;         // vetor b de entrada (n)
     int * p;            // vetor de permutação de colunas (m)
     double * norma;     // quadrado das normas 2 das colunas (m)
-    double * escala;    // norma infinita das colunas (m)
     double * gamma;     // gammas (m)
     int posto;          // posto de A
+    double escala;      // escalamento inicial do problema
 
     recebe_entrada(&n, &m, A, b);
-    aloca_auxiliares(m, p, norma, escala, gamma);
-    pre_processa(n, m, A, p, norma, escala);
-    posto = decompoe_qr(n, m, A, b, p, norma, escala, gamma);
+    aloca_auxiliares(m, p, norma, gamma);
+    escala = pre_processa(n, m, A, p, norma);
+    posto = decompoe_qr(n, m, A, b, p, norma, gamma);
+
+    printf("%lf %d\n", escala, posto);
 }
