57929181d0344d4507d84daea890462a8e7826c3
Victor Sena Molero <victorsenam@gmail.com>
2015-11-22 23:45:06 -0200

    final provavelmente

--- mac300/ep3/codigo/exemplos/criaTestes.sh
+++ mac300/ep3/codigo/exemplos/criaTestes.sh
@@ -4,3 +4,4 @@ make gerador/gen
 ./gerador/gen < ./gerador/ex1.inp > testes/poly1.dat
 ./gerador/gen < ./gerador/ex2.inp > testes/poly2.dat
 ./gerador/gen < ./gerador/ex3.inp > testes/poly3.dat
+./gerador/gen < ./gerador/ex4.inp > testes/poly4.dat
