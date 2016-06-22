5cbb977ef2cb20dca6a90360a7dd53158af8f5bd
Victor Sena <victorsenam@gmail.com>
2016-06-22 15:05:51 -0300

    Se você quer amor, chegue aqui

--- monitoria/mac323/4-2-33/helpers/commentToGrades.sh
+++ monitoria/mac323/4-2-33/helpers/commentToGrades.sh
@@ -10,5 +10,5 @@ cd result
 
 for aluno in $(find $pat -maxdepth 1 -mindepth 1 -type d);
 do
-    cat $aluno/comment.txt $aluno/judge.out $aluno/judge.log > $initial/grades/$(basename $aluno).txt
+    cat $aluno/comment.txt $initial/helpers/warning.txt $aluno/judge.out $aluno/judge.log > $initial/grades/$(basename $aluno).txt
 done
