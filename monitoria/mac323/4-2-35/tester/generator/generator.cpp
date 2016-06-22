e526a78b6128607838156aa3fefa9a169e3799bb
Victor Sena Molero <victorsenam@gmail.com>
2016-06-22 09:59:53 -0300

    Pra confundir

---
+++ monitoria/mac323/4-2-35/tester/generator/generator.cpp
@@ -0,0 +1,57 @@
+#include "../testlib.h"
+#include <cstdio>
+#include <map>
+#include <algorithm>
+#include <cassert>
+#include <random>
+
+typedef long long int ll;
+
+const int N = 1000000;
+
+// params
+ll n, m;
+std::set<std::pair<int, int> > s;
+std::pair<int, int> aux;
+int qtd[N];
+int p[N];
+
+int get_int (char ** argv) {
+    int res;
+    sscanf(argv[0], "%d", &res);
+    return res;
+}
+
+int main (int argc, char ** argv) {
+    registerGen(argc, argv, 1);
+    
+    n = rnd.next(get_int(argv+1), get_int(argv+2));
+    m = rnd.next(get_int(argv+3), get_int(argv+4));
+    m = std::max(m, 0ll);
+    m = std::min(m, n*(n-1));
+    int q = n;
+
+    for (int i = 0; i < n; i++) {
+        p[i] = i;
+        qtd[i] = n-1;
+    }
+
+    printf("%lld %lld\n", n, m);
+    while (s.size() < m) {
+        int ind = rnd.next(q);
+        aux = std::pair<int, int>(p[ind], rnd.next(n));
+
+        if (s.find(aux) == s.end()) {
+            s.insert(aux);
+            qtd[aux.first]--;
+
+            if (qtd[aux.first] == 0) {
+                std::swap(p[ind], p[q-1]);
+                q--;
+            }
+        }
+    }
+
+    for (std::pair<int, int> v : s)
+        printf("%d %d\n", v.first, v.second);
+}
---
+++ monitoria/mac323/4-2-35/tester/judge/judge.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+# Parâmetros
+# 1 - Pasta da solução a ser testada
+# 2 (opcional) - Pasta contendo os testes a serem rodados
+# 3 (opcional) - Pasta contendo uma solulção válida
+
+# important vars
+DIR="$( cd "$( dirname "$0" )" && cd ../../ && pwd )/"
+LOG="judge.log"
+RES="judge.out"
+OUT="judge_output/"
+TMPOUT="judge_output.txt"
+
+# parameters
+if [[ $# -eq 0 ]];
+then
+    echo "NO ARGUMENTS PROVIDED"
+    exit 1
+fi
+folder=$1
+cases=${DIR}${2-'tester/cases/'}
+anwsers=${DIR}${3-'tester/solution/anwser/'}
+
+# going to wanted folder
+cd $folder
+
+# resetting
+echo "" > $LOG
+echo "" > $RES
+rm -r $OUT &>> $LOG
+mkdir $OUT &>> $LOG
+r_ac=0
+r_tl=0
+r_wa=0
+r_re=0
+r_tm=0
+
+# compiling
+rm CorretorDoVictaum.java
+rm Out.java
+make ${DIR}tester/checker/checker &>> $LOG
+cp -n ${DIR}tester/standart/* . &>> $LOG
+cp -n ${DIR}tester/judge/.gitignore . &>> $LOG
+rm *.class
+javac -cp .:algs4.jar:stdlib.jar *.java &>> $LOG
+
+toexec=failed
+if [ -a CoreVertices.class ];
+then
+    toexec=CoreVertices
+fi
+
+# second compile method
+if [ $toexec == failed ];
+then
+    echo "Second Compile Method" &>> $LOG
+
+    rm *.class
+    echo -e "import edu.princeton.cs.algs4.*;\nimport java.util.*;\n$(cat WordDAG.java)" > WordDAG.java
+    javac -cp .:algs4.jar:stdlib.jar *.java &>> $LOG
+    
+    if [ -a CoreVertices.class ];
+    then
+        toexec=CoreVertices
+    fi
+fi
+
+
+if [ $toexec != failed ];
+then
+    testpath="$cases/unique_test.in"
+    
+    #touch saida.txt
+    #chmod 777 saida.txt
+    #tail -f saida.txt > $TMPOUT 2> /dev/null &
+    (time java -cp .:algs4.jar:stdlib.jar $toexec < $testpath > $TMPOUT) 2>> $LOG
+    run_status=$?
+
+    case_num=0
+    lines_read=-1
+    case_lines=0
+
+    cat $TMPOUT | while read line
+    do
+        ((lines_read++))
+        if [[ $lines_read -le 0 ]];
+        then
+            continue
+        fi
+
+        if [[ ${line:0:1} == "-" ]]
+        then
+            curr_out=$(printf "${OUT}%03d_case.out" $case_num)
+            
+            cat $TMPOUT | head -n $lines_read | tail -n $case_lines > $curr_out
+
+            ((case_num++))
+            case_lines=-1
+            printf "."
+        fi
+        ((case_lines++))
+    done
+    printf "\n"
+
+    for casepath in $(find $OUT/*);
+    do
+        testcase=$(basename $casepath)
+        testcase=${testcase%.*}
+        veredict="F"
+        
+        echo "====== $testcase =========" >> $LOG
+        
+        if grep -q "TEMPO DE EXECUCAO EXCEDIDO" $casepath;
+        then
+            ((r_tl++))
+            echo "Tempo Excedido" >> $LOG
+            veredict="T"
+        elif grep -q "TEMPO DE MONTAGEM EXCEDIDO" $casepath;
+        then
+            ((r_tm++))
+            echo "Tempo de Montagem Excedido" >> $LOG
+            veredict="M"
+        elif grep -q "ERRO DE EXECUCAO" $casepath;
+        then
+            ((r_re++))
+            echo "Erro de Execução" >> $LOG
+            veredict="R"
+        else
+            sort ${OUT}${testcase}.out -o ${OUT}${testcase}.out
+            checker_output=$( ${DIR}tester/checker/checker $testpath ${OUT}${testcase}.out ${DIR}tester/solution/answer/${testcase}.out 2>&1)
+            echo $checker_output >> $LOG
+            if [[ ${checker_output:0:1} == "o" ]]
+            then
+                ((r_ac++))
+                echo "OK" >> $LOG
+                veredict="."
+            else
+                ((r_wa++))
+                echo "Resposta Errada" >> $LOG
+                veredict="W"
+            fi
+        fi
+
+        printf $veredict
+        printf $veredict >> $RES
+    done;
+else
+    echo "COMPILATION ERROR" >> $RES
+fi
+
+printf "\n"
+printf "\n" >> $RES
+
+echo "CORREÇÃO AUTOMÁTICA" >> $RES
+echo "Quantidade de Testes | Veredito" >> $RES
+echo "$r_ac | OK" >> $RES
+echo "$r_tm | Erro de Montagem (o grafo dessa query não pode ser montado)" >> $RES
+echo "$r_tl | Tempo de Execução Excedido" >> $RES
+echo "$r_wa | Resposta Errada" >> $RES
+echo "$r_re | Erro de Execução" >> $RES
